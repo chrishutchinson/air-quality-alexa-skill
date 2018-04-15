e9d5dc18479f45bb24935c20bca4f7e2a4592248
Chris Hutchinson <chris_hutchinson@me.com>
2018-04-15 18:01:51 +0100

    Update wording

--- app/lang/en.js
+++ app/lang/en.js
@@ -1,15 +1,15 @@
 module.exports = {
   launch: () =>
-    `You can ask for the latest air quality data for a given location, like so: "Alexa ask Air Quality what the air quality is today in Liverpool". If you need more details on the air quality index, ask: "Alexa ask Air Quality what the air quality index scale is".`,
+    `You can ask for the latest air quality data for your location, like so: "Alexa, ask Air Quality for an update". You can also ask by providing a specific location, like so: "Alexa, ask Air Quality about air quality in Liverpool". If you need more details on the air quality index, ask: "Alexa, ask Air Quality what the air quality index scale is".`,
 
   description: () =>
     `This description is from the Department for Environment Food and Rural Affairs website. The Daily Air Quality Index tells you about levels of air pollution and provides recommended actions and health advice. The index is numbered 1 to 10 and divided into four bands, low (1) to very high (10), to provide detail about air pollution levels in a simple way, similar to the sun index or pollen index.`,
 
   help: () =>
-    `This Alexa skill provides Defra air quality data for locations around the UK using the air quality index scale. You can ask for the latest air quality data for a given location, like so: "Alexa ask Air Quality what the air quality is today in Liverpool". If the skill finds multiple stations in the location you provide, it will list them after giving a response, allowing you to ask for the air quality at a more specific location. If you need more details on the air quality index, ask: "Alexa ask Air Quality what the air quality index scale is". So, how can I help?`,
+    `This Alexa skill provides Defra air quality data for locations around the UK using the air quality index scale. You can ask for the latest air quality data for your location, like so: "Alexa, ask Air Quality for an update". You can also ask by providing a specific location, like so: "Alexa, ask Air Quality about air quality in Liverpool". If the skill finds multiple stations in the location you provide, it will list them after giving a response, allowing you to ask for the air quality at a more specific location. If you need more details on the air quality index, ask: "Alexa ask Air Quality what the air quality index scale is". So, how can I help?`,
 
   unknownIntent: () =>
-    `I'm sorry, I didn't understand that intent. You can ask for the latest air quality data for a given location, like so: "Alexa ask Air Quality what the air quality is today in Liverpool". If you need more details on the air quality index, ask: "Alexa ask Air Quality what the air quality index scale is".`,
+    `I'm sorry, I didn't understand that. You can ask for the latest air quality data for your location, like so: "Alexa, ask Air Quality for an update". You can also ask by providing a specific location, like so: "Alexa, ask Air Quality about air quality in Liverpool". If you need more details on the air quality index, ask: "Alexa ask Air Quality what the air quality index scale is".`,
 
   invalidCity: () =>
     `Please provide a city or location to check the air quality for. For example: "Ask air quality what is the air quality today in Liverpool".`,
